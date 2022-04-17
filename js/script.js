57d0d0bac4084fd966e31a726ab17fde89d91dec
sin1917 <103310011+sin1917@users.noreply.github.com>
2022-04-17 23:27:44 +0900

    デザイン適用準備のためhtml構造を変更

--- js/script.js
+++ js/script.js
@@ -1,95 +1,139 @@
-function func1() {
+function func1(inputBox, destElement) {
     //jsondataの取得
-    var txt = document.getElementById("inputBox").value;
+    const textData = document.getElementById(inputBox).value;
 
     try {
-        var data = JSON.parse(txt);
+        var jsonData = JSON.parse(textData);
     } catch (err) {
         //console.log("not json");
-        var element = document.getElementById("dist");
-        element.textContent = "入力データが正常ではありません";
+        document.getElementById(destElement).textContent="入力データが正常ではありません";
         return false;
     }
 
-    ClearElementContents("dist");
-    func3(data, "dist");
+    //入力されたデータが正しいかチェック
+    if (false == CheckInputText(jsonData)) {
+        //console.log("data fault");
+        document.getElementById(destElement).textContent="入力データが正常ではありません";
+        return false;
+    }
+
+    ClearElementContents(destElement);
+    func3(jsonData, destElement);
+
+    ClearTextbox(inputBox);
 }
 
-function func2(jsondata) {
-
-}
+function CheckInputText(inputJson) {
+    //構造確認 (艦隊と個艦の情報があればいいはず？)
+    if ("api_deck_port" in inputJson && "api_ship" in inputJson) {
+        //console.log("ok");
+        return true;
+    }
+    
+    //console.log("false");
+    return false;
+}   
 
 function func3(jsonData, elementId) {
-    var apiShip = jsonData.api_ship;
-    var apiPort = jsonData.api_deck_port;
-    var apiShipData = api_mst_ship;
+    const apiShip = jsonData.api_ship;
+    const apiPort = jsonData.api_deck_port;
     var parent = document.getElementById(elementId);
 
+    var block = document.createElement("div");
+    block.classList.add("fleet-data");
 
     for (let index = 0; index < apiPort.length; index++) {
-        var block = document.createElement("div");
+        var fleet = document.createElement("div");
+        fleet.classList.add("fleet")
 
         //
-        var fleetName = document.createElement("p");
-        fleetName.textContent = apiPort[index].api_name;
-        block.appendChild(fleetName);
+        var fleetName = document.createElement("div");
+        fleetName.classList.add("fleet-name");
+        fleetName.textContent = (index+1)+". "+apiPort[index].api_name;
+        fleet.appendChild(fleetName);
 
         //
         var uniqueIDs = apiPort[index].api_ship;
-        var shipList = document.createElement("ol");
+        var shipList = document.createElement("div");
+        shipList.classList.add("fleet-ship-list");
         for (let i = 0; i < uniqueIDs.length; i++) {
-            var Item = document.createElement("li");
-            if (uniqueIDs[i] != -1) {
-                var uniqueData = apiShip.find((v) => v.api_id == uniqueIDs[i]);
-                //console.log(uniqueData);
-
-                var lv = uniqueData.api_lv;
-                var cnd = uniqueData.api_cond;
-                var shipId = uniqueData.api_ship_id;
-                var buff = apiShipData.find((v) => v.api_id == uniqueData.api_ship_id);
-                var name = buff.api_name;
-                // console.log(buff);
-
-                Item.textContent = "lv." + lv + " / " + name + " / " + "cond:" + cnd;
-            } else {
-                Item.textContent = "-";
-            }
-            shipList.appendChild(Item);
+            var listItem = func4(uniqueIDs[i],apiShip);
+            shipList.appendChild(listItem);
         }
-        block.appendChild(shipList);
+        fleet.appendChild(shipList);
 
         //
-        parent.appendChild(block);
+        block.appendChild(fleet);
     }
-
+    parent.appendChild(block)
 }
 
+function func4(uniqueID, apiShip){
+    var Item = document.createElement("div");
+    Item.classList.add("fleet-ship-data");
 
-//sort_id確認
-function func5() {
-    var data = api_data.api_mst_ship;
-    var sortNumArray = [];
-    for (let i = 0; i < data.length; i++) {
-        const sortid = data[i].api_sort_id;
-        if (sortid != 0)
-            sortNumArray.push(sortid);
-    }
-    sortNumArray.sort(compare);
+    if (uniqueID != -1) {
+        var uniqueData = apiShip.find((v) => v.api_id == uniqueID);
 
-    var resultArray = [];
-    for (let i = 0; i < sortNumArray.length; i++) {
-        const sortid = sortNumArray[i];
-        var x = data.find((v) => v.api_sort_id == sortNumArray[i]);
-        resultArray.push({ "name": x.api_name, "id": x.api_id, "sortid": sortid });
+        const lv = uniqueData.api_lv;
+        const cnd = uniqueData.api_cond;
+        const name = GetShipNameById(uniqueData.api_ship_id);
+
+        var shipLv = document.createElement("div");
+        shipLv.classList.add("fleet-ship-lv");
+        shipLv.textContent = "lv." + lv;
+        var shipName = document.createElement("div");
+        shipName.classList.add("fleet-ship-name");
+        shipName.textContent = name;
+        var shipCond = document.createElement("div");
+        shipCond.classList.add("fleet-ship-cond");
+        shipCond.textContent = "cond:" + cnd;
+
+        Item.appendChild(shipLv);
+        Item.appendChild(shipName);
+        Item.appendChild(shipCond);
+
+        //Item.textContent = "lv." + lv + " / " + name + " / " + "cond:" + cnd;
+    } else {
+        Item.textContent = "-";
     }
-    console.log(resultArray);
+
+    return Item;
 }
-function compare(a, b) {
-    var r = 0;
-    if (a < b) { r = -1; }
-    else if (a > b) { r = 1; }
 
-    return r;
+// //sort_id確認
+// function func0() {
+//     var data = api_mst_ship;
+//     var sortNumArray = [];
+//     for (let i = 0; i < data.length; i++) {
+//         const sortid = data[i].api_sort_id;
+//         if (sortid != 0)
+//             sortNumArray.push(sortid);
+//     }
+//     sortNumArray.sort(compare);
+
+//     var resultArray = [];
+//     for (let i = 0; i < sortNumArray.length; i++) {
+//         const sortid = sortNumArray[i];
+//         var x = data.find((v) => v.api_sort_id == sortNumArray[i]);
+//         resultArray.push({ "name": x.api_name, "id": x.api_id, "sortid": sortid });
+//     }
+//     console.log(resultArray);
+// }
+// function compare(a, b) {
+//     var r = 0;
+//     if (a < b) { r = -1; }
+//     else if (a > b) { r = 1; }
+
+//     return r;
+// }
+
+//=============================================================
+
+//ship_idから名前を取得
+function GetShipNameById(shipId){
+    const shipData = api_mst_ship.find((v) => v.api_id == shipId);
+    return shipData.api_name;
 }
 
 //==============================================================
@@ -106,4 +150,3 @@ function ClearTextbox(id) {
     var textbox = document.getElementById(id);
     textbox.value = "";
 }
-
